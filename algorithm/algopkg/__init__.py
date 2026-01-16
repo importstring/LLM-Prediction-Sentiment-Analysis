29054684bb0db5593f5eed592963131cfdd56b8d
Simon Bergeron <simon.bergeron.home@gmail.com>
2026-01-16 14:37:49 -0500

    Minor personal preference changes to __init__.py

--- algorithm/algopkg/__init__.py
+++ algorithm/algopkg/__init__.py
@@ -2,4 +2,8 @@ from .config import settings
 from .api.service import run_agent_once
 from .onboarding import run_onboarding
 
-__all__ = ["settings", "run_agent_once", "run_onboarding"]
+__all__ = [
+    "settings", 
+    "run_agent_once", 
+    "run_onboarding"
+]
